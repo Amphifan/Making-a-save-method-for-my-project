2d46cd72749a5ce4be39d4a32b8d1b25dd685853
Amphifan <91407600+Amphifan@users.noreply.github.com>
2021-09-26 08:27:00 +0100

    Add files via upload

---
+++ SaveButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SaveButton : MonoBehaviour
+{
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData2 data = SaveSystem.LoadPlayer();
+
+        level = data.level;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+
+
+
+
+    }
+
+
+}
